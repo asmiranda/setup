94d9da815eecdb6b62f7a10ebd62dc3db1ee10de
Alex <niq77avie@yahoo.com>
2020-08-27 18:03:24 +0800

    Init

--- installation/5-install-mysql.sh
+++ installation/5-install-mysql.sh
@@ -6,6 +6,6 @@ cd ${setup_dir}
 sudo mkdir ${setup_tmp}/mysql
 cd ${setup_tmp}/mysql
 
-docker run --name dynamiko-mysql -e MYSQL_ROOT_PASSWORD=Miran!11 -d mysql:8.0.21
+docker run --name dynamiko-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=Miran!11 -d mysql:8.0.21
 
 cd ${setup_dir}
