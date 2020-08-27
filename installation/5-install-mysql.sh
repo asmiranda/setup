2099a0bbc7ea5830a9b998d4d466ab83552ce1f2
Alex <niq77avie@yahoo.com>
2020-08-27 12:50:22 +0800

    Init

--- installation/5-install-mysql.sh
+++ installation/5-install-mysql.sh
@@ -19,7 +19,7 @@ sudo cp ${setup_dir}/conf.d/mysqld.conf /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo systemctl start mysql
 sudo systemctl enable mysql
 
-# need to do manual setup for root access in webmin, then  rerun the script
+# need to do manual setup for root access in webmin, then rerun the script
 sudo mysql -h "localhost" -u "root" "-pMiran!11" < ${setup_dir}/helper/mysql-init.sql
 
 cd ${setup_dir}
