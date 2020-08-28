73aae01461d52b2186a416f4c4d0bb22552cb813
Alex <niq77avie@yahoo.com>
2020-08-28 15:32:17 +0800

    Init

--- installation/a7-install-coturn.sh
+++ installation/a7-install-coturn.sh
@@ -10,8 +10,8 @@ sudo ufw allow 3478/tcp
 sudo ufw allow 3478/udp
 sudo ufw allow 49152:65535/tcp
 
-sudo apt-get update
-sudo apt-get install coturn
+sudo apt-get update -y
+sudo apt-get install coturn -y
 
 
 sudo ufw enable
