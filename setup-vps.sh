bd8dc1484abcdcfcfc414415c98a1bebe41adc1e
Alex <niq77avie@yahoo.com>
2020-08-28 16:10:09 +0800

    Init

--- setup-vps.sh
+++ setup-vps.sh
@@ -13,7 +13,7 @@ sudo mkdir ${setup_tmp}
 installation/1-install-getsource.sh
 # installation/2-install-webmin.sh
 # installation/3-install-docker.sh
-# installation/4-install-portainer.sh
+installation/4-install-portainer.sh
 # installation/5-install-mysql.sh
 # note: need to manually connect as  root to mysql thru webmin before resume.
 
@@ -21,10 +21,10 @@ installation/1-install-getsource.sh
 # installation/8-install-php.sh
 # installation/a1-install-wordpress.sh
 # installation/a2-install-dynamiko-docker.sh
-# installation/a3-install-nginxproxy.sh
+installation/a3-install-nginxproxy.sh
 # installation/a4-install-python.sh
 # installation/a5-install-certbot.sh
-installation/a7-install-coturn.sh
+# installation/a7-install-coturn.sh
 
 cd ${setup_dir}
 echo "-- setup dir --- "
