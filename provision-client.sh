1f2df8f8afba60248abc7c7f03556a7ba7a2e9d1
Alex <niq77avie@yahoo.com>
2020-09-04 13:21:14 +0800

    Init

--- provision-client.sh
+++ provision-client.sh
@@ -29,10 +29,10 @@ sudo mkdir ${setup_tmp}/migration
 # installation/7-install-nginx.sh
 # installation/8-install-php.sh
 
-installation/a1-install-wordpress.sh
+# installation/a1-install-wordpress.sh
 installation/a2-install-dynamiko-docker.sh
-installation/a3-install-nginxproxy.sh
-installation/a4-install-python.sh
+# installation/a3-install-nginxproxy.sh
+# installation/a4-install-python.sh
 # installation/a5-install-certbot.sh
 # installation/a7-install-coturn.sh
 # installation/a8-install-firewall.sh
