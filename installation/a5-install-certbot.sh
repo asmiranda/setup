ed1883f7cae0c90274b3b5d20fb075d5d76eacd9
Alex <niq77avie@yahoo.com>
2020-09-04 13:50:10 +0800

    Init

--- installation/a5-install-certbot.sh
+++ installation/a5-install-certbot.sh
@@ -8,10 +8,9 @@ cd ${setup_tmp}/certbot
 sudo systemctl stop nginx.service
 
 sudo apt-get install certbot -y
-sudo rm -r /etc/letsencrypt/live/*
 
 certbot certonly --cert-name dynamikosoft.com -d ${subdomain}dynamikosoft.com -m aiamemiranda@gmail.com --standalone --agree-tos
-sudo mv /etc/letsencrypt/live/dynamikosoft.com* /etc/letsencrypt/live/dynamikosoft.com
+# sudo mv /etc/letsencrypt/live/dynamikosoft.com* /etc/letsencrypt/live/dynamikosoft.com
 
 sudo systemctl restart nginx.service
 
