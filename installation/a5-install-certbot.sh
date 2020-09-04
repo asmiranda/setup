990df6f833f3e6df057e6f24877bc731d088fee5
Alex <niq77avie@yahoo.com>
2020-09-04 12:57:29 +0800

    Init

--- installation/a5-install-certbot.sh
+++ installation/a5-install-certbot.sh
@@ -8,7 +8,8 @@ cd ${setup_tmp}/certbot
 sudo systemctl stop nginx.service
 
 sudo apt-get install certbot -y
-certbot certonly --cert-name dynamikosoft.com -d *.dynamikosoft.com -m aiamemiranda@gmail.com --standalone --agree-tos
+# sudo certbot certonly --cert-name dynamikosoft.com -d *.dynamikosoft.com -m aiamemiranda@gmail.com --standalone --agree-tos
+sudo certbot --cert-name dynamikosoft.com --authenticator standalone --installer nginx -d *.dynamikosoft.com -m aiamemiranda@gmail.com --pre-hook "service nginx stop" --post-hook "service nginx stop"
 
 sudo systemctl restart nginx.service
 
