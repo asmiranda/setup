a8a7fd44dd6523fa3f67c10d26dba405891db2dc
Alex <niq77avie@yahoo.com>
2020-09-04 13:32:03 +0800

    Init

--- installation/a5-install-certbot.sh
+++ installation/a5-install-certbot.sh
@@ -8,6 +8,8 @@ cd ${setup_tmp}/certbot
 sudo systemctl stop nginx.service
 
 sudo apt-get install certbot -y
+sudo rm /etc/letsencrypt/live/dynamikosoft.com/*
+
 certbot certonly --cert-name dynamikosoft.com -d ${subdomain}dynamikosoft.com -m aiamemiranda@gmail.com --standalone --agree-tos
 
 sudo systemctl restart nginx.service
--- provision-client.sh
+++ provision-client.sh
@@ -33,7 +33,7 @@ sudo mkdir ${setup_tmp}/migration
 installation/a2-install-dynamiko-docker.sh
 # installation/a3-install-nginxproxy.sh
 # installation/a4-install-python.sh
-# installation/a5-install-certbot.sh
+installation/a5-install-certbot.sh
 # installation/a7-install-coturn.sh
 # installation/a8-install-firewall.sh
 
