1f5f65d50c8fb8460cdf1a2bc6cbd5a293edd1d5
Alex <niq77avie@yahoo.com>
2020-09-05 09:03:32 +0800

    Init

--- installation/client-deploy-epltrc.sh
+++ installation/client-deploy-epltrc.sh
@@ -5,8 +5,8 @@ export setup_tmp=/root/tmp
 
 cd ${setup_dir}/helper
 
-docker-compose -f ./compose/epltrc.docker-compose.yml stop
-docker-compose -f ./compose/epltrc.docker-compose.yml up -d
+docker-compose -f ./epltrc.docker-compose.yml stop
+docker-compose -f ./epltrc.docker-compose.yml up -d
 
 sudo cp ${setup_dir}/conf.d/epltrc-nginx-proxy.conf /etc/nginx/sites-available/epltrc-nginx-proxy.conf
 sudo ln -s /etc/nginx/sites-available/epltrc-nginx-proxy.conf /etc/nginx/sites-enabled/epltrc-nginx-proxy.conf
