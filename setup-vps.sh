e3a8f3849a98e9e6435b70556cdc0c1ae011a5f7
Alex <niq77avie@yahoo.com>
2020-09-05 09:03:46 +0800

    Init

--- setup-vps.sh
+++ setup-vps.sh
@@ -21,7 +21,7 @@ installation/1-install-getsource.sh
 # installation/8-install-php.sh
 # installation/a1-install-wordpress.sh
 installation/a2-install-dynamiko-docker.sh
-installation/a3-install-nginxproxy.sh
+# installation/a3-install-nginxproxy.sh
 # installation/a4-install-python.sh
 # installation/a5-install-certbot.sh
 # installation/a7-install-coturn.sh
