068b4303f05e1904ca4de88a1172af00aa248d42
Alex <niq77avie@yahoo.com>
2020-09-14 23:26:01 +0800

    Init

--- 05-portainer/setup.sh
+++ 05-portainer/setup.sh
@@ -8,8 +8,10 @@ sudo mkdir ${setup_tmp} && cd ${setup_dir}
 echo "************Start Portainer Setup************"
 
 cd ${setup_tmp}/portainer
+# sudo docker rm -vf portainer
 
 sudo docker volume create portainer_data
-sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+
+sudo docker run -d -p 9000:9000 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -v /etc/letsencrypt/live/dynamikosoft.com:/certs:ro portainer/portainer --ssl --sslcert /certs/fullchain.pem --sslkey /certs/privkey.pem
 
 cd ${setup_dir}
