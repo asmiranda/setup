35383cdac310462877590e2b0233b889b4f715a2
Alex <niq77avie@yahoo.com>
2020-09-20 14:47:49 +0800

    Init

--- 18-email/setup.sh
+++ 18-email/setup.sh
@@ -7,9 +7,11 @@ sudo mkdir ${setup_tmp} && cd ${setup_dir}
 
 echo "************Start Email Setup************"
 
-cd /
 sudo useradd -p $(openssl passwd -1 Miran!11) -s /bin/bash -d /home/admin/ -m -G sudo admin
-sudo mkdir mailu && cd mailu
+cd /
+sudo mkdir mailu 
+cp . /mailu
+cd /mailu
 sudo docker-compose -p mailu exec admin flask mailu admin admin dynamikosoft.com Miran!11
 
 cd ${setup_dir}
